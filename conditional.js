42daeb86a0ed1a71cccec4b08dcbe177db5e7652
meow-codes201 <humairat291@gmail.com>
2025-11-15 19:22:51 +0530

    conditionals

---
+++ conditional.js
@@ -0,0 +1,33 @@
+let age = 8;
+
+if(age>=18){
+    console.log('vote allowed');
+    
+}
+else if(age<18 && age>0){
+    console.log('vote not allowed');
+    
+}
+else{
+    console.log('invalid age');
+    
+}
+
+console.log('CAN YOU DRIVE?');
+
+let age1 = 15;
+if(age1>=16){
+    console.log('You can drive');
+}
+else if(age1>=15){
+    console.log('Almost There!');
+    
+}
+else if(age1<16 && age1>0){
+    console.log('You cannot drive');
+    
+}
+else{
+    console.log("invalid age");
+    
+}
