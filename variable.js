839af220e849c2d03e231c669a814cd765138750
meow-codes201 <humairat291@gmail.com>
2025-11-15 12:26:50 +0530

    variables

---
+++ variable.js
@@ -0,0 +1,13 @@
+let v1 = "Hello";
+console.log(v1);
+v1 += " world!";
+console.log(v1);
+let calc = 2+2 * 10 - 10/5;
+console.log(calc); //20
+console.log(calc+8); //add more to variable - 28
+let res = calc +8; //this gives 28 too
+console.log(res);
+// alert(v1);
+
+
+
