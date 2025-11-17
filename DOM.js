23c79f1fdfab5508e1618b63a99e730fe19b2c9e
meow-codes201 <humairat291@gmail.com>
2025-11-17 19:49:12 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,19 @@
+// document.body.innerHTML = 'hello';
+// document.title = 'My new title';
+// console.log(document.title);
+// console.log(document.body.innerHTML);
+// console.log(typeof document.body.innerHTML); //string
+// console.log(document.body.innerHTML.length);
+// console.log(document.body.innerHTML.toUpperCase());
+// console.log(document.body.innerHTML.indexOf('hello'));
+// console.log(document.body.innerHTML.replace('hello','hi'));
+// console.log(document.body.innerHTML.split(' '));
+
+// document.body.innerHTML = '<h1>HELLO </h1>';
+document.querySelector('button').innerHTML = 'clique';
+document.querySelector('button').style.backgroundColor = 'lightblue';
+document.querySelector('button').style.color = 'white';
+document.querySelector('button').style.borderColor= 'lightblue';
+
+document.querySelector('.js-btn').innerHTML = 'CLICK ME TOO';
+document.querySelector('.js-btn').style.backgroundColor = 'hotpink';
