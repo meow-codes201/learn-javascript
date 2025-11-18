acca9fbb69334eb10ced705e2b99fabc7490577f
meow-codes201 <humairat291@gmail.com>
2025-11-18 21:43:26 +0530

    added cart quantity logic

--- javascript-amazon-project-main/scripts.js
+++ javascript-amazon-project-main/scripts.js
@@ -77,10 +77,14 @@ button.addEventListener('click',()=>{
         productId:productId,
         quantity:1
     })
-    
     }
-    
-    console.log(cart);
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+cartQuantity += item.quantity;
+    })
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    // console.log(cartQuantity);
+    // console.log(cart);
 
     
 })
