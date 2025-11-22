ce517a47ac037587a0e508b0c657983a9ab2aa7a
meow-codes201 <humairat291@gmail.com>
2025-11-22 09:38:33 +0530

    classes and construcctor

--- classes.js
+++ classes.js
@@ -1,3 +1,33 @@
-class Product{
-    
+class Products{
+    constructor(name,price){
+        this.name = name,
+        this.price = price
+    }
+    displayProduct(){
+        console.log(this.name);
+        console.log(this.price.toFixed(2));
+        
+        
+    }
+
+    calculateTotal(salesTax){
+        return (this.price + (this.price * salesTax)).toFixed(2);
+    }
 }
+const p1 = new Products('shirt',19.99);
+p1.displayProduct();
+const p2 = new Products('Pants',22.50);
+p2.displayProduct();
+const salesTax = 0.05;
+console.log(p2.calculateTotal(salesTax)
+);
+console.log(p1.calculateTotal(salesTax));
+
+//static 
+class Meth{
+    static PI = 3.14;
+}
+console.log(Meth.PI); // ACCESS THROUGH VLASS DIRECTLY
+const m1 = new Meth; // DOESNT WORK
+console.log(m1.PI); //UNDEFINED
+
