8c48547f8897ebb439bf9dabc4826619f66328ac
meow-codes201 <humairat291@gmail.com>
2025-11-22 23:41:14 +0530

    getters and setters

--- getters-setters.js
+++ getters-setters.js
@@ -39,10 +39,99 @@ get area(){
 
 
 }
-const rect = new Rectangle(5,9);
-// console.log(rect.width + " " + rect.height);
-rect.width = 10;
-rect.height = 18;
-console.log(rect.height);
-console.log(rect.height);
-console.log(rect.area);
+class Person{
+    constructor(firstName,lastName,age){
+        this.firstName= firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+set firstName(newfn){
+    if(typeof newfn === 'string' && newfn.length>0){
+        this._firstName = newfn;
+    }
+    else
+        console.error('must be written in alphabets');
+    
+}
+
+set lastName(newln){
+    if(typeof newln === 'string' && newln.length>0){
+        this._lastName = newln;
+    }
+    else
+        console.error('must be written in alphabets');
+        
+}
+
+
+set age(newAge){
+    if(typeof newAge === 'number' && newAge>0){
+        this._age = newAge;
+    }
+    else
+        console.error('age = non negative');
+        
+}
+get firstName(){
+    return this._firstName;
+}
+get lastName(){
+    return this._lastName;
+}
+get age(){
+    return this._age;
+}
+get fullName(){
+    return `${this._firstName} ${this._lastName}`;
+}
+
+
+}
+
+// const person1 = new Person(420,69,'kebab')
+// console.log(person1.firstName);
+// console.log(person1.lastName);
+// console.log(person1.age);
+
+
+
+
+const person1 = new Person('Horrid','Henry',18)
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.fullName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const rect = new Rectangle(5,9);
+// // console.log(rect.width + " " + rect.height);
+// rect.width = 10;
+// rect.height = 18;
+// console.log(rect.height);
+// console.log(rect.height);
+// console.log(rect.area);
+
+
