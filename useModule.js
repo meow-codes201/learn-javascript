d8926173024e14c76f4485601738957312acc7d5
meow-codes201 <humairat291@gmail.com>
2025-11-24 19:12:35 +0530

    use math module (external)

---
+++ useModule.js
@@ -0,0 +1,8 @@
+import {PI, getCircumference,getArea,getVolume} from './math-util.js';
+console.log(PI);
+console.log(getCircumference(10).toFixed(2));
+const ar1 = getArea(10);
+const v1 = getVolume(10);
+
+console.log(ar1.toFixed(2));
+console.log(v1.toFixed(2));
