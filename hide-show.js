25573470b7636effed939748cc5172dd5e4f34c4
meow-codes201 <humairat291@gmail.com>
2025-11-26 17:39:00 +0530

    hide-show program

---
+++ hide-show.js
@@ -0,0 +1,14 @@
+const myBtn = document.getElementById("myBtn");
+const myImg = document.getElementById("myImg");
+
+
+myBtn.addEventListener("click",(event)=>{
+    if(myImg.style.visibility === 'hidden'){
+        myImg.style.visibility = 'visible';
+        myBtn.textContent = 'hide'
+    }
+        else{
+    myImg.style.visibility = 'hidden';
+    myBtn.textContent = 'show';
+}
+})
